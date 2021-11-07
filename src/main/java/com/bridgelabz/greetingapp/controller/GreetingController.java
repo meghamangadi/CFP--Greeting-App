344c41769fd34817224cbb7146cf0fe0e6704757
megha angadi <meghamangadi@gmail.com>
2021-11-07 23:13:08 +0530

    Added : Created Spring App for Hello Messages using different HTTP Methods

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,5 +1,30 @@
 package com.bridgelabz.greetingapp.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.greetingapp.model.Greeting;
+
+@RestController
 public class GreetingController {
+
+	private static final String template = "Hello ,%s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("greeting/{name}")
+	public Greeting greetings(@PathVariable String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 
 }
